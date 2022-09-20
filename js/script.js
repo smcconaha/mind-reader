10be87dbeedc5733aaec7a6830b84b76aef70d09
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-09-20 21:29:20 +0000

    Added the following files: styles.css, pseudo.md, script.js, gitignore, and index.html.  Populated md file with notes from class discussion regarding state objects and some basic functions.

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* What are my objects?
+Header
+Next/Reveal
+Helper
+Example
+Reset/Go
+Pages
+    exampletext: ""
+    helpertext: ""
+    page #: int
+    btn text: ""
+    header text: ""
+    
+
+*/
