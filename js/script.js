e5458a0891d961de0d26b45842092aded0e1f59d
smcconaha <steve.mcconaha@student.awesomeinc.org>
2022-09-23 02:26:07 +0000

    Added icon buttons and adjusted col and container class minimum heights

--- js/script.js
+++ js/script.js
@@ -9,46 +9,46 @@ let state = {
         example: '',
         helper: '',
         resetBtn: '',
-        goBtn: '<i class="bi bi-forward"></i>',
+        goBtn: 'GO <i class="bi bi-forward"></i>',
         symbols: '',
         },
         {
         pageNum: 1,
         header: "Pick a number from 01 -99",
-        nextBtn: 'Next',
+        nextBtn: 'Next <i class="bi bi-forward"></i>',
         example: '',
         helper: 'when you have your number click next',
-        resetBtn: '<i class="bi bi-recycle"></i>',
+        resetBtn: 'Reset <i class="bi bi-recycle"></i>',
         goBtn: '',
         symbols: '',            
         },
         {
         pageNum: 2,
         header: "Add both digits together to get a new number",
-        nextBtn: 'Next',
+        nextBtn: 'Next <i class="bi bi-forward"></i>',
         example: '14 is 1 + 4 = 5',
         helper: 'click next to proceed',
-        resetBtn: '<i class="bi bi-recycle"></i>',
+        resetBtn: 'Reset <i class="bi bi-recycle"></i>',
         goBtn: '',
         symbols: '',            
         },
         {
         pageNum: 3,
         header: "Subtract your new number from the original number",
-        nextBtn: 'Next',
+        nextBtn: 'Next <i class="bi bi-forward"></i>',
         example: 'Ex: 14 - 5 = 9',
         helper: 'click next to proceed',
-        resetBtn: 'Reset<i class="bi bi-recycle"></i>',
+        resetBtn: 'Reset <i class="bi bi-recycle"></i>',
         goBtn: '',
         symbols: '',            
         },
         {
         pageNum: 4,
         header: symbolFun (),
-        nextBtn: 'Reveal',
+        nextBtn: 'Reveal <i class="bi bi-question-circle"></i>',
         example: 'Find your new number',
         helper: 'Note the symbol beside the number',
-        resetBtn: '<i class="bi bi-recycle"></i>',
+        resetBtn: 'Reset <i class="bi bi-recycle"></i>',
         goBtn: '',
         symbols: '',            
         },
@@ -58,7 +58,7 @@ let state = {
         nextBtn: '',
         example: `Your symbol is ${answerArr}`,
         helper: 'Note the symbol beside the number',
-        resetBtn: '<i class="bi bi-recycle"></i>',
+        resetBtn: 'Reset <i class="bi bi-recycle"></i>',
         goBtn: '',
         symbols: '',            
         },
@@ -79,7 +79,7 @@ const header = document.getElementById('header');
 function updatePage () {
     header.textContent = state.pages[state.currentPage].header;
     helper.textContent = state.pages[state.currentPage].helper;
-    nextBtn.textContent = state.pages[state.currentPage].nextBtn;
+    nextBtn.innerHTML = state.pages[state.currentPage].nextBtn;
     goBtn.innerHTML = state.pages[state.currentPage].goBtn;
     resetBtn.innerHTML = state.pages[state.currentPage].resetBtn;
     example.textContent = state.pages[state.currentPage].example;
